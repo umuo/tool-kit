0d7b67710d8317192ebc8b8165c58d77d9c76bf6
gitsilence <gitsilence@gmail.com>
2026-05-30 15:22:30 +0800

    build: Enable minify and filter ABIs to shrink release APK size

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,11 +19,15 @@ android {
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        ndk {
+            abiFilters += listOf("armeabi-v7a", "arm64-v8a")
+        }
     }
 
     buildTypes {
         release {
-            isMinifyEnabled = false
+            isMinifyEnabled = true
+            isShrinkResources = true
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
